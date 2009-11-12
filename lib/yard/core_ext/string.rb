479f8700bb0faebedac55128272db86c4fb7b90d
Noah Davis and Luke Melia <developers@weplay.com>
2009-11-12 14:45:36 -0500

    Remove String core extensions which conflict with ActiveSupport versions.
    Appeared to be unused.

--- lib/yard/core_ext/string.rb
+++
@@ -1,9 +0,0 @@
-class String
-  def underscore
-    gsub(/([a-z])([A-Z])/, '\1_\2').downcase 
-  end
-  
-  def camelcase
-    gsub(/([a-z])_([a-z])/i) { $1 + $2.upcase }.sub(/^(.)/) { $1.upcase } 
-  end
-end
